0f7da48ce773efd88b8adb37ca7f70e7d89464e7
Zeynep Sude Turan <157915520+zeynepsturan@users.noreply.github.com>
2025-09-06 17:06:28 +0300

    Update global.cpp

--- src/global.cpp
+++ src/global.cpp
@@ -1,7 +1,7 @@
 #include <string>
 using namespace std;
 
-string col_to_str(int num){
+string col_to_str(int num){ //converts column name to string
     string str;
     int carry=0;
     if(num>26){ //like AA,AB
@@ -15,10 +15,11 @@ string col_to_str(int num){
     return str;
 }
 
-int col_to_int(string str){
+int col_to_int(string str){ //converts column name to integer
     int num = 0;
     for (char c:str){
         num=num *26+(c-'A'+1);
     }
     return num;
+
 }
